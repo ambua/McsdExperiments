b69c88c98781820232083d48f9109251d27def99
Alltag <Alltag@WORKSTATIONPC>
2017-01-08 20:33:16 +0100

    local storage save.

---
+++ McsdExperiments/Scripts/Custom/LocalStorage.js
@@ -0,0 +1,22 @@
+﻿(function ()
+{
+    $(document).ready(function () {
+        var storage = window.localStorage;
+        $("#Button1").click(function (evt) {
+            storage.setItem("fullname", $("#TextBox1").val());
+            storage.setItem("email", $("#TextBox2").val());
+            $("#Label3").html("Data saved to LocalStorage!");
+            evt.preventDefault();
+        });
+
+        $("#Button2").click(function (evt) {
+            var data = {};
+            data.FullName = storage.getItem("fullname");
+            data.Email = storage.getItem("email");
+            $("#HiddenField1").val(JSON.stringify(data));
+        });
+
+
+    });
+})();
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # McsdExperiments
+
+Local Storage:
+http://www.codeguru.com/csharp/.net/two-ways-of-passing-html5-web-storage-data-to-asp.net.htm
